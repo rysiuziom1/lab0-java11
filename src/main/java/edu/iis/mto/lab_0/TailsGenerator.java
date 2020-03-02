9a38378885f3a102ae5a579062df0b64dfb5d6bc
jacek.wychowałek <215927@edu.p.lodz.pl>
2020-03-02 18:10:12 +0100

    edit gitignore and implement tails method

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,12 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        List<String> stringList = new ArrayList<>();
+        for (int i = 0; i < value.length(); i++) {
+            stringList.add(value.substring(i));
+        }
+        stringList.add("");
+        return stringList;
     }
 
 }
